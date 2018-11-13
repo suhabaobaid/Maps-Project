f8aa84d9f37723bbdef4cd7fe2bc701f70cb28e8
suhabaobaid <suha.baobaid@gmail.com>
2018-11-13 18:22:44 +0300

    Added data for the location

--- js/data.js
+++ js/data.js
@@ -1,1 +1,46 @@
 /* Model Data */
+
+const LOCATIONS = [
+	{
+		name: 'Octopus Book & Cafe',
+		location: {
+			lat: 36.88202233161344,
+			lng: 30.708858464141475,
+		},
+	},
+	{
+		name: 'Çay-Tea\'s Lunchroom & Deco Home',
+		location: {
+			lat: 36.88380687475678,
+			lng: 30.705753415083123,
+		},
+	},
+	{
+		name: 'Starbucks',
+		location: {
+			lat: 36.88274438051892,
+			lng: 30.70879340171814,
+		},
+	},
+	{
+		name: 'DOSPRESSO Bombty Coffee & Donut',
+		location: {
+			lat: 36.881793,
+			lng: 30.707895,
+		},
+	},
+	{
+		name: 'DOSPRESSO Bombty Coffee & Donut',
+		location: {
+			lat: 36.881793,
+			lng: 30.707895,
+		},
+	},
+	{
+		name: 'Tchibo',
+		location: {
+			lat: 36.881256630420886,
+			lng: 30.707944140819144,
+		},
+	},
+];
